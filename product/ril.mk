5fff75c7e0ed98c26da00eee4ef722c9e65d0760
vlw <vlwwwwww@gmail.com>
2017-03-04 15:58:58 +0300

    Update RIL config
    Change-Id: I6d24f84b5683550719a5ac6cbdf95b349be55712

--- product/ril.mk
+++ product/ril.mk
@@ -5,23 +5,17 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libsec-ril.so \
-    rild.libargs=-d /dev/smd0 \
-    persist.radio.rat_on=combine \
-    ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=9 \
-    ro.product_ship=true \
-    ro.ril.transmitpower=true \
-    persist.radio.add_power_save=1 \
-    persist.radio.no_wait_for_card=1 \
+    keyguard.no_require_sim=true \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.use_se_table_only=1 \
-    persist.radio.jbims=1 \
-    telephony.lteOnGsmDevice=1 \
-    ro.ril.telephony.mqanelements=6 \
-    ro.telephony.ril.config=simactivation \
-    ro.telephony.ril_class=SamsungQcom3GDSRIL \
-    ro.telephony.samsung.realcall=true
+    DEVICE_PROVISIONED=1 \
+    persist.radio.override_psvoice=true \
+    ro.telephony.samsung.realcall=true \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libpath2=/system/lib/libsec-ril-dsds.so \
+    persist.radio.lte_vrte_ltd=1 \
+    persist.radio.add_power_save=1 \
+    telephony.lteOnCdmaDevice=0
 
 
 # Netmgrd
@@ -29,10 +23,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=false \
     persist.data.netmgrd.qos.enable=false
 
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=0 \
+    persist.cne.dpm=0 \
+    persist.dpm.feature=0
+
 # Samsung libril shim library
-PRODUCT_PACKAGES += \
-    libril_shim
+#PRODUCT_PACKAGES += \
+#    libril_shim
 
 # Samsung rild
-PRODUCT_PACKAGES += \
-    rild
+#PRODUCT_PACKAGES += \
+#    rild
