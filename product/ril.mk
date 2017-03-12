3f85e9952067f6918a1fd158ae87e1d677d9dc81
vlw <vlwwwwww@gmail.com>
2017-03-13 01:40:14 +0300

    Update ril config for BQB2
    Change-Id: Idc4f06fd43bd39c0c465d843b9255ce564a8acaa

--- product/ril.mk
+++ product/ril.mk
@@ -5,24 +5,28 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libargs=-d /dev/smd0 \
     ro.telephony.default_network=9 \
-    keyguard.no_require_sim=true \
-    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.rat_on=combine \
+    ril.subscription.types=NV,RUIM \
     DEVICE_PROVISIONED=1 \
     persist.radio.override_psvoice=true \
     ro.telephony.samsung.realcall=true \
-    rild.libpath=/system/lib/libsec-ril.so \
-    rild.libpath2=/system/lib/libsec-ril-dsds.so \
-    persist.radio.lte_vrte_ltd=1 \
-    persist.radio.add_power_save=1 \
-    telephony.lteOnCdmaDevice=0 \
+    keyguard.no_require_sim=true \
     ro.telephony.ril_class=SamsungA3RIL
-
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=false \
     persist.data.netmgrd.qos.enable=false
+
+#SAMP_SPCM
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.config.samp_spcm_enable=true \
+    sys.config.spcm_db_enable=true \
+    sys.config.spcm_db_launcher=true \
+    sys.config.spcm_preload_enable=true \
+    sys.config.samp_ppm_enable=false
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
