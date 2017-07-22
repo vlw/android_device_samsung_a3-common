6442e651786f7043956dbfbcbbb372a66d62de21
vlw <vlwwwwww@gmail.com>
2017-07-22 13:12:10 +0300

    a3: Remove not supported touchscreen DT2W
    Change-Id: If454c1b34174e3a27cf33f211bbaf00b302c801a

--- board/other.mk
+++ board/other.mk
@@ -6,9 +6,6 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/a3-common/include
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-# Touchscreen
-TARGET_TAP_TO_WAKE_NODE := "/sys/class/sec/sec_touchscreen/wake_gesture"
-
 #SU
 WITH_SU := true
 
