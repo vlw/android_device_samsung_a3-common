b88b2b059a09964c6c334a015b7a53a23560dc47
vlw <vlwwwwww@gmail.com>
2017-07-23 17:22:31 +0300

    a3: product: OTA: Remove unnecessary files
    * Remove lines with copy non-exists files
    
    Change-Id: I5e819f2c447436ff339cec7d28accb1bd9cb4aeb

--- product/OTA.mk
+++ product/OTA.mk
@@ -1,7 +1,4 @@
 # OTA scripts
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/releasetools/variant_hook.sh:install/bin/variant_hook.sh \
-	$(LOCAL_PATH)/releasetools/installend/copy_nfc_configs.sh:install/bin/installend/copy_nfc_configs.sh \
-	$(LOCAL_PATH)/releasetools/installend/copy_variant_blobs.sh:install/bin/installend/copy_variant_blobs.sh \
-	$(LOCAL_PATH)/releasetools/installend/update_baseband.sh:install/bin/installend/update_baseband.sh \
 	$(LOCAL_PATH)/releasetools/installend/update_device_name.sh:install/bin/installend/update_device_name.sh
