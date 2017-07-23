6cc8505ddbc0b37516467b090555ded4446ba854
vlw <vlwwwwww@gmail.com>
2017-07-23 16:51:25 +0300

    a3: product: ril: Remove IMS support prop
    * This device not support VoLTE, so we're don't need
      to have any IMS stuff. Also now this is unused prop.
    
    Change-Id: I84f644a241f2996e322d2878dfff34bb007e07fb

--- product/ril.mk
+++ product/ril.mk
@@ -3,8 +3,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.rat_on=combine \
     ro.ril.transmitpower=true \
     persist.radio.no_wait_for_card=1 \
-    persist.radio.use_se_table_only=1 \
-    persist.radio.jbims=1
+    persist.radio.use_se_table_only=1
 
 # Samsung libril shim library
 PRODUCT_PACKAGES += \
