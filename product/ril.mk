276ecbc9b8576be875fabd5b93ac38f1b1405d4a
vlw <vlwwwwww@gmail.com>
2017-07-23 16:36:51 +0300

    a3: product: ril: Cleanup
    Change-Id: I6793506be531459e88af3815adcc0be9d7c67215

--- product/ril.mk
+++ product/ril.mk
@@ -1,15 +1,10 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.rat_on=combine \
-    ro.telephony.default_network=9 \
-    ro.product_ship=true \
     ro.ril.transmitpower=true \
     persist.radio.no_wait_for_card=1 \
     persist.radio.use_se_table_only=1 \
-    persist.radio.jbims=1 \
-    telephony.lteOnGsmDevice=1 \
-    ro.ril.telephony.mqanelements=6 \
-    ro.telephony.ril.config=simactivation
+    persist.radio.jbims=1
 
 # Samsung libril shim library
 PRODUCT_PACKAGES += \
